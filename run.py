732169f4a7ef6bbcf80e521e131accdb744e276e
Brad <bball.yoder@gmail.com>
2020-10-04 20:01:06 -0400

    removed some personal comments

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 from bs4 import BeautifulSoup
 import requests, TTS, time, sys
 
-#todo add fun sounds for threes and such, add params for different modes (verbose, succinct, scores and misses only)
-
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
 def get_play_table(url):
